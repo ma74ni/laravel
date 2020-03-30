9969da16b1d9ccd35641fc8a9b1596d21da383b4
Diego Paredes <ma74ny@gmail.com>
2020-03-29 20:56:07 -0500

    Agregar rutas

--- curso-styde/routes/web.php
+++ curso-styde/routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return 'Inicio';
+});
+Route::get('/usuario/nuevo', function() {
+    return "Crear nuevo usuario";
+});
+Route::get('/usuario/{id}', function($id) {
+    return "Hola usuario: {$id}";
+});
+Route::get('/saludo/{name}/{nickname?}', function($name, $nickname = null) {
+    $name = ucfirst($name);
+    if($nickname){
+        return "Bienvenido {$name}, tu nickname es: {$nickname}";
+    } else {
+        return "Bienvenido {$name}";
+    }
 });
